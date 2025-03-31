3a6e7d81a4d7dd316ab7ebe64f8d9bd52d783df1
enzogabALF <150981047+enzogabALF@users.noreply.github.com>
2025-03-31 10:39:10 -0300

    dos casos de pruebas 1) ir a inasistencias y confirmar que te encuentras ahi 2) ir a aulas virtuales confirmando que no te encuentras en inasistencias

--- src/login.test.ts
+++ src/login.test.ts
@@ -7,47 +7,6 @@ dotenv.config({ path: path.resolve(__dirname, '../utils/iniciodesecion.env') });
 
 jest.setTimeout(120000); // 120 segundos
 
-test('Debería retornar true cuando se logra iniciar sesión', async () => {
-    const automation = new LoginAutomation();
-
-    try {
-        await automation.launchBrowser();
-        await automation.navigateToLoginPage(
-            'https://sistemacuenca.ucp.edu.ar/alumnosnotas/Default.aspx?ReturnUrl=%2falumnosnotas%2fProteccion%2fInscripcionesExamenes.aspx%3fSel%3d2&Sel=2'
-        );
-        await automation.performLogin();
-        await automation.validateLoginSuccess();
-
-        // Si no lanza errores, el inicio de sesión fue exitoso
-        expect(true).toBe(true);
-    } finally {
-        await automation.closeBrowser();
-    }
-});
-
-test('Debería retornar true cuando se abre el panel de cursado después de iniciar sesión', async () => {
-    const automation = new LoginAutomation();
-
-    try {
-        await automation.launchBrowser();
-        await automation.navigateToLoginPage(
-            'https://sistemacuenca.ucp.edu.ar/alumnosnotas/Default.aspx?ReturnUrl=%2falumnosnotas%2fProteccion%2fInscripcionesExamenes.aspx%3fSel%3d2&Sel=2'
-        );
-        await automation.performLogin();
-        await automation.validateLoginSuccess();
-
-        // Intentar abrir el panel de cursado
-        const result = await automation.abrirPanelCursado('#ctl00_PanelCursado_header');
-
-        // Validar que el resultado sea true
-        expect(result).toBe(true);
-    } catch (error) {
-        console.error('Error en el test de abrir el panel de cursado:', error);
-        throw error;
-    } finally {
-        await automation.closeBrowser();
-    }
-});
 
 test('Debería retornar true cuando se navega a Inasistencias después de abrir el panel de cursado', async () => {
     const automation = new LoginAutomation();
@@ -60,6 +19,25 @@ test('Debería retornar true cuando se navega a Inasistencias después de abrir 
         await automation.performLogin();
         await automation.validateLoginSuccess();
         const result = await automation.abrirPanelCursado('a[href="Inasistencias.aspx?Sel=1"]');
+
+        // Validar que el resultado sea true
+        expect(result).toBe(true);
+    } finally {
+        await automation.closeBrowser();
+    }
+});
+
+test('Debería retornar true cuando no se navega a Inasistencias después de abrir el panel de cursado', async () => {
+    const automation = new LoginAutomation();
+
+    try {
+        await automation.launchBrowser();
+        await automation.navigateToLoginPage(
+            'https://sistemacuenca.ucp.edu.ar/alumnosnotas/Default.aspx?ReturnUrl=%2falumnosnotas%2fProteccion%2fInscripcionesExamenes.aspx%3fSel%3d2&Sel=2'
+        );
+        await automation.performLogin();
+        await automation.validateLoginSuccess();
+        const result = await automation.abrirPanelCursado('a[href="AsistenciaVirtual.aspx?Sel=1"]');
 
         // Validar que el resultado sea true
         expect(result).toBe(true);
